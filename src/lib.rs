d4e4e8153bf2186aea3d721f4e8b5068e605c853
Kaneyuki Hiromasa <matsugen2511@gmail.com>
2019-08-16 21:32:49 +0900

    アニメーション追加関数実装 amethyst で読み込ませるとロードが終わらない？

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod part;
-use amethyst::animation::AnimationHierarchyPrefab;
+use amethyst::animation::{AnimationHierarchyPrefab, AnimationPrefab, AnimationSetPrefab};
 use amethyst::assets::{PrefabData, ProgressCounter};
 use amethyst::core::Transform;
 use amethyst::derive::*;
@@ -16,6 +16,8 @@ use serde::*;
 pub struct SpriteAnimation {
     sprite_hierarchy: Option<AnimationHierarchyPrefab<SpriteRender>>,
     transform_hierarchy: Option<AnimationHierarchyPrefab<Transform>>,
+    sprite_animation: Option<AnimationSetPrefab<usize, SpriteRender>>,
+    transform_animation: Option<AnimationSetPrefab<usize, Transform>>,
     cells: Option<SpriteSheetPrefab>,
     render: Option<SpriteRenderPrefab>,
     transform: Transform,
@@ -30,13 +32,27 @@ impl SpriteAnimation {
         SpriteAnimation {
             sprite_hierarchy: None,
             transform_hierarchy: None,
+            sprite_animation: None,
+            transform_animation: None,
             cells,
             render,
             transform,
         }
     }
 
-    pub fn set_transform_hierarchy(&mut self, transform_hierarchy: AnimationHierarchyPrefab<Transform>){
+    pub fn set_transform_hierarchy(
+        &mut self,
+        transform_hierarchy: AnimationHierarchyPrefab<Transform>,
+    ) {
         self.transform_hierarchy = transform_hierarchy.into();
+    }
+
+    pub fn add_transform_animation(&mut self, animation: AnimationPrefab<Transform>) {
+        if self.transform_animation.is_none() {
+            self.transform_animation = AnimationSetPrefab::default().into();
+        }
+        let transform_animation = self.transform_animation.as_mut().unwrap();
+        let id = transform_animation.animations.len();
+        transform_animation.animations.push((id, animation));
     }
 }
