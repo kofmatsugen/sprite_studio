bf857ce76e2bd2166c0593b971a315c7f78a89e4
Kaneyuki Hiromasa <matsugen2511@gmail.com>
2020-05-03 11:54:55 +0900

    ネームスペースを短縮化

--- src/traits.rs
+++ src/traits.rs
@@ -2,54 +2,31 @@ pub mod animation_file;
 pub(crate) mod interpolate;
 pub mod translate_animation;
 use serde::{Deserialize, Serialize};
+use std::{fmt::Debug, hash::Hash};
 
 // トレイトのエイリアス定義
 // 大小比較，ハッシュ化，送信安全性とデバッグ表示が保証されればキーとして使える
 pub trait AnimationKey:
-    'static
-    + Send
-    + Sync
-    + std::fmt::Debug
-    + Ord
-    + std::hash::Hash
-    + Copy
-    + Clone
-    + Serialize
-    + for<'de> Deserialize<'de>
+    'static + Send + Sync + Debug + Ord + Hash + Copy + Serialize + for<'de> Deserialize<'de>
 {
 }
 
 impl<T> AnimationKey for T where
-    T: 'static
-        + Send
-        + Sync
-        + std::fmt::Debug
-        + Ord
-        + std::hash::Hash
-        + Copy
-        + Clone
-        + Serialize
-        + for<'de> Deserialize<'de>
+    T: 'static + Send + Sync + Debug + Ord + Hash + Copy + Serialize + for<'de> Deserialize<'de>
 {
 }
 
 // ユーザーデータ，シリアライズ，送信安全性とデバッグ表示が保証されればユーザーデータとして使える
 pub trait AnimationUser:
-    'static + Send + Sync + Serialize + for<'de> Deserialize<'de> + std::fmt::Debug + Copy
+    'static + Send + Sync + Serialize + for<'de> Deserialize<'de> + Debug + Copy
 {
 }
 
 impl<T> AnimationUser for T where
-    T: 'static + Send + Sync + Serialize + std::fmt::Debug + for<'de> Deserialize<'de> + Copy
+    T: 'static + Send + Sync + Serialize + Debug + for<'de> Deserialize<'de> + Copy
 {
 }
 
-pub trait FileId:
-    'static + Send + Sync + Ord + std::hash::Hash + std::fmt::Debug + Clone + Copy
-{
-}
+pub trait FileId: 'static + Send + Sync + Ord + Hash + Debug + Copy {}
 
-impl<T> FileId for T where
-    T: 'static + Send + Sync + Ord + std::hash::Hash + std::fmt::Debug + Clone + Copy
-{
-}
+impl<T> FileId for T where T: 'static + Send + Sync + Ord + std::hash::Hash + std::fmt::Debug + Copy {}
